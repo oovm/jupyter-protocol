6caaf6876a919ebe87a03b895ba4d6f7834c7956
Aster <aster@vers.site>
2022-10-25 19:56:00 +0000

    🔧 Fix export transparent image

--- projects/jupyter/src/value_type/for_image.rs
+++ projects/jupyter/src/value_type/for_image.rs
@@ -1,7 +1,21 @@
 use crate::{Executed, JupyterTheme};
-use image::{codecs::png::PngEncoder, ColorType, DynamicImage, ImageEncoder};
+use image::{codecs::png::PngEncoder, ColorType, DynamicImage, ImageEncoder, RgbaImage};
 use serde_json::Value;
-use std::fmt::Formatter;
+
+impl Executed for RgbaImage {
+    fn mime_type(&self) -> String {
+        "image/png".to_string()
+    }
+
+    fn as_json(&self, _: JupyterTheme) -> Value {
+        let mut buf = Vec::new();
+        let writer = PngEncoder::new(&mut buf);
+        writer.write_image(self.as_raw(), self.width(), self.height(), ColorType::Rgba8).unwrap();
+        let data = base64::encode(&buf);
+        let data_url = format!("data:image/png;base64,{}", data);
+        Value::String(format!(r#"<img src="{}"/>"#, data_url,))
+    }
+}
 
 impl Executed for DynamicImage {
     fn mime_type(&self) -> String {
@@ -9,18 +23,6 @@ impl Executed for DynamicImage {
     }
 
     fn as_json(&self, theme: JupyterTheme) -> Value {
-        let bg = match theme {
-            JupyterTheme::Light => image::Rgba([255, 255, 255, 255]),
-            JupyterTheme::Dark => image::Rgba([0, 0, 0, 255]),
-        };
-        let mut buf = Vec::new();
-        let mut writer = PngEncoder::new(&mut buf);
-        writer.write_image(self.to_rgb8().as_raw(), self.width(), self.height(), ColorType::Rgba8).unwrap();
-        let data = base64::encode(&buf);
-        let data_url = format!("data:image/png;base64,{}", data);
-        Value::String(format!(
-            r#"<img src="{}" style="background-color:#{:02x}{:02x}{:02x};" />"#,
-            data_url, bg[0], bg[1], bg[2]
-        ))
+        self.to_rgba8().as_json(theme)
     }
 }
