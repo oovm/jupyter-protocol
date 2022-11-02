49f793fe1e17511c7634e84481051116fee7d568
Aster <aster@vers.site>
2022-11-02 03:04:47 +0000

    🐤 Add evaluator algebra

--- projects/jupyter-calculator/src/engine/mod.rs
+++ projects/jupyter-calculator/src/engine/mod.rs
@@ -1,22 +1,93 @@
-enum Expr {
-    /// 整数类型
-    Lit(i32),
-    /// 取反操作
-    Neg(Box<Expr>),
-    /// 加法操作
-    Add(Box<Expr>, Box<Expr>),
+use num::traits::FloatErrorKind;
+
+pub trait ElementaryAlgebra {
+    type Tag<T>;
+    fn integer(&mut self, v: i64) -> Self::Tag<f64>;
+    fn plus(&mut self, b1: Self::Tag<f64>, b2: Self::Tag<f64>) -> Self::Tag<f64>;
+    fn minus(&mut self, b1: Self::Tag<f64>, b2: Self::Tag<f64>) -> Self::Tag<f64>;
+    fn times(&mut self, b1: Self::Tag<f64>, b2: Self::Tag<f64>) -> Self::Tag<f64>;
+    fn divide(&mut self, b1: Self::Tag<f64>, b2: Self::Tag<f64>) -> Self::Tag<f64>;
 }
 
-impl Expr {
-    fn lit(i: i32) -> Expr {
-        Expr::Lit(i)
+#[derive(Clone, Debug, Default)]
+pub struct Evaluator {
+    count: usize,
+}
+
+impl Evaluator {
+    pub fn increment(&mut self) {
+        self.count += 1;
+    }
+}
+
+impl ElementaryAlgebra for Evaluator {
+    type Tag<T> = Result<f64, FloatErrorKind>;
+
+    fn integer(&mut self, v: i64) -> Self::Tag<f64> {
+        self.increment();
+        Ok(v as f64)
     }
 
-    fn neg(r: Expr) -> Expr {
-        Expr::Neg(Box::new(r))
+    fn plus(&mut self, b1: Self::Tag<f64>, b2: Self::Tag<f64>) -> Self::Tag<f64> {
+        self.increment();
+        Ok(b1? + b2?)
     }
 
-    fn add(r1: Expr, r2: Expr) -> Expr {
-        Expr::Add(Box::new(r1), Box::new(r2))
+    fn minus(&mut self, b1: Self::Tag<f64>, b2: Self::Tag<f64>) -> Self::Tag<f64> {
+        self.increment();
+        Ok(b1? - b2?)
+    }
+
+    fn times(&mut self, b1: Self::Tag<f64>, b2: Self::Tag<f64>) -> Self::Tag<f64> {
+        self.increment();
+        Ok(b1? * b2?)
+    }
+
+    fn divide(&mut self, b1: Self::Tag<f64>, b2: Self::Tag<f64>) -> Self::Tag<f64> {
+        self.increment();
+        Ok(b1? / b2?)
+    }
+}
+
+pub struct Printer;
+
+impl ElementaryAlgebra for Printer {
+    type Tag<T> = String;
+
+    fn integer(&mut self, v: i64) -> Self::Tag<f64> {
+        format!("{}", v)
+    }
+
+    fn plus(&mut self, b1: Self::Tag<f64>, b2: Self::Tag<f64>) -> Self::Tag<f64> {
+        format!("({} + {})", b1, b2)
+    }
+
+    fn minus(&mut self, b1: Self::Tag<f64>, b2: Self::Tag<f64>) -> Self::Tag<f64> {
+        format!("({} - {})", b1, b2)
+    }
+
+    fn times(&mut self, b1: Self::Tag<f64>, b2: Self::Tag<f64>) -> Self::Tag<f64> {
+        format!("({} * {})", b1, b2)
+    }
+
+    fn divide(&mut self, b1: Self::Tag<f64>, b2: Self::Tag<f64>) -> Self::Tag<f64> {
+        format!("({} / {})", b1, b2)
+    }
+}
+
+pub trait SqrtAlgebra: ElementaryAlgebra {
+    fn sqrt(&mut self, b: Self::Tag<f64>) -> Self::Tag<f64>;
+}
+
+impl SqrtAlgebra for Evaluator {
+    fn sqrt(&mut self, b: Self::Tag<f64>) -> Self::Tag<f64> {
+        self.increment();
+        Ok(b?.sqrt())
+    }
+}
+
+impl SqrtAlgebra for Printer {
+    fn sqrt(&mut self, b: Self::Tag<f64>) -> Self::Tag<f64> {
+        format!("sqrt({})", b)
     }
 }
--- projects/jupyter-calculator/tests/main.rs
+++ projects/jupyter-calculator/tests/main.rs
@@ -1,4 +1,32 @@
+use calculator::{Evaluator, Printer, SqrtAlgebra};
+
 #[test]
 fn ready() {
     println!("it works!")
+}
+
+fn program<L>(interpreter: &mut L) -> L::Tag<f64>
+where
+    L: SqrtAlgebra,
+{
+    let lhs = interpreter.integer(2);
+    let rhs = interpreter.integer(3);
+    let o1 = interpreter.plus(lhs, rhs);
+    interpreter.sqrt(o1)
+}
+
+fn parse<L: SqrtAlgebra>(input: &str) -> L::Tag<f64> {
+    todo!()
+}
+
+#[test]
+fn main() {
+    let mut l1 = Printer {};
+    let mut l2 = Evaluator::default();
+
+    let test1_p = program(&mut l1);
+
+    println!("{:?}", test1_p);
+    let test1_b = program(&mut l2);
+    println!("{:?}", test1_b);
 }
