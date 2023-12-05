0259c4eba82b1d1e6b91da7c504fe3b6febd7f02
Aster <aster@vers.site>
2023-12-05 22:07:00 +0800

    Adjust dependency compatibility

--- projects/jupyter-types/src/third_party/mod.rs
+++ projects/jupyter-types/src/third_party/mod.rs
@@ -8,56 +8,17 @@ mod for_ndarray;
 pub use mathml_core::MathML;
 #[cfg(feature = "ndarray")]
 pub use ndarray::{Array1, Array2};
-
 #[cfg(feature = "svg")]
 pub use svg::Document;
 #[cfg(feature = "url")]
 pub use url::Url;
-
+mod for_number;
+mod for_string;
 #[cfg(feature = "svg")]
 mod for_svg;
 
 use crate::{Executed, JupyterContext};
 use serde_json::Value;
-
-impl Executed for bool {
-    fn mime_type(&self) -> String {
-        "text/plain".to_string()
-    }
-
-    fn as_json(&self, _: &JupyterContext) -> Value {
-        // bool not support in Jupyter
-        Value::String(self.to_string())
-    }
-}
-
-impl Executed for String {
-    fn mime_type(&self) -> String {
-        "text/plain".to_string()
-    }
-
-    fn as_json(&self, _: &JupyterContext) -> Value {
-        self.clone().into()
-    }
-}
-
-impl Executed for char {
-    fn mime_type(&self) -> String {
-        "text/plain".to_string()
-    }
-    fn as_json(&self, _: &JupyterContext) -> Value {
-        Value::String(self.to_string())
-    }
-}
-
-impl<'a> Executed for &'a str {
-    fn mime_type(&self) -> String {
-        "text/plain".to_string()
-    }
-    fn as_json(&self, _: &JupyterContext) -> Value {
-        self.to_string().into()
-    }
-}
 
 impl Executed for Value {
     fn mime_type(&self) -> String {
@@ -77,26 +38,5 @@ impl Executed for Url {
 
     fn as_json(&self, _: &JupyterContext) -> Value {
         Value::String(format!(r#"<a href="{}">{}</a>"#, self, self))
-    }
-}
-
-impl Executed for i32 {
-    fn mime_type(&self) -> String {
-        "text/plain".to_string()
-    }
-
-    fn as_json(&self, _: &JupyterContext) -> Value {
-        // number not support in Jupyter
-        Value::String(self.to_string())
-    }
-}
-
-impl Executed for f64 {
-    fn mime_type(&self) -> String {
-        "text/plain".to_string()
-    }
-
-    fn as_json(&self, _: &JupyterContext) -> Value {
-        Value::String(self.to_string())
     }
 }
